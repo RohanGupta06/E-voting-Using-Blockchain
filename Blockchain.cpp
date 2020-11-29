d5aa948c993b66fe7a7c927399e73bf5af95e774
Rohan gupta <500067051@stu.upes.ac.in>
2020-11-29 13:05:15 +0530

    Update Blockchain.cpp

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -5,97 +5,101 @@
 #include <sstream>
 #include <cstring>
 using namespace std;
-template <typename Temp>
+template <typename T>
 class Blockchain {
   private:
-    template <typename typ>
-    class Block 
-    {
+    template <typename U>
+    class Block {
       public:
-        typ* mesg_data;
-        string mesg_prevHash;
-        string mesg_hash;
-        Block *mesg_next;
+        U* m_data;
+        string m_prevHash;
+        string m_hash;
+        Block *m_next;
 
-        Block(string prevHash, typ* data, Block *next) 
-        {
-          mesg_prevHash = prevHash;
-          mesg_data = data;
-          mesg_next = next;
+        Block(string prevHash, U* data, Block *next) {
+          m_prevHash = prevHash;
+          m_data = data;
+          m_next = next;
           stringstream s;
-          s << mesg_prevHash << mesg_data;
-          mesg_hash = sha256(s.str());
-        }
-        
-        Block() //generic block
-        {
-          mesg_prevHash = sha256("genesis block");
-          mesg_next = 0;
-          mesg_data = new typ();
-          calcHash(mesg_prevHash);
+          //hash is the hash of previous hash + data
+          s << m_prevHash << m_data;
+          m_hash = sha256(s.str());
         }
 
-        ~Block() 
-        {
-          delete mesg_data;
-          mesg_data = 0;
+        //if class, T/U has to have default constructor
+        //creates genesis block that has next pointing to 0
+        Block() {
+          m_prevHash = sha256("genesis block");
+          m_next = 0;
+          m_data = new U();
+          calcHash(m_prevHash);
         }
 
-        void calcHash(string prevHash) 
-        {
+        ~Block() {
+          delete m_data;
+          m_data = 0;
+        }
+
+        void calcHash(string prevHash) {
           stringstream s;
-          mesg_prevHash = prevHash;
-          s << mesg_prevHash << mesg_data;
-          mesg_hash = sha256(s.str());
+          m_prevHash = prevHash;
+          s << m_prevHash << m_data;
+          m_hash = sha256(s.str());
         }
 
-        typ getData() const 
-        {
-          return *mesg_data;
+        U getData() const {
+          return *m_data;
         }
 
-        friend ostream& operator<<(ostream& os, const Block& b) 
-        {
-          os << "Data: " << b.getData() << endl << "Hash: " << b.mesg_hash << endl << "Previous Hash: " << b.mesg_prevHash;
+        friend ostream& operator<<(ostream& os, const Block& b) {
+          os << "Data: " << b.getData() << endl << "Hash: " << b.m_hash << endl << "Previous Hash: " << b.m_prevHash;
           return os;
         }
     };
 
-    Block<Typ> *mesg_head;
-    Block<Typ> *mesg_tail;
-    int mesg_size;
+    Block<T> *m_head;
+    Block<T> *m_tail;
+    int m_size;
 
   public:
-    Blockchain() 
-    {
-      mesg_head = new Block<Typ>();
-      mesg_tail = mesg_head;
+    Blockchain() {
+      m_head = new Block<T>();
+      m_tail = m_head;
       string lastHash = getLastHash();
+      //create binary file with last hash
       ofstream outfile("lastHash.dat", ios::binary);
       outfile.write(lastHash.c_str(),lastHash.size());
       outfile.close();
-      mesg_size=0; 
+      m_size=0;
+      /*TESTING BINARY FILE (Reading)
+      ifstream infile("lastHash.dat", ios::binary);
+      string hash;
+      hash.resize(m_lastHash.size());
+      infile.read(&hash[0], m_lastHash.size());
+      infile.close();
+      cout<<"\nFile data: "<<hash<<endl;*/
     }
     
-    int getSize() 
-    {
-      return mesg_size;
+    int getSize() {
+      return m_size;
     }
 
-    string getLastHash() 
-    {
-      Block<Typ> *current = mesg_head;
-      string currentHash = current->mesg_hash;
-      while (current->mesg_next != 0) {
-        current->mesg_next->calcHash(currentHash);
-        current = current->mesg_next;
+    string getLastHash() {
+      Block<T> *current = m_head;
+      string currentHash = current->m_hash;
+      while (current->m_next != 0) {
+        current->m_next->calcHash(currentHash);
+        current = current->m_next;
       }
 
-      return current->mesg_hash;
+      return current->m_hash;
     }
-    
-    bool verify()   //compares file's last hash to the chain's last hash
-    {
+
+    //TODO:
+    /*~Blockchain(){}
+    */
+    //compares file's last hash to the chain's last hash
+    bool verify() {
       //read from file
       string lastHash = getLastHash();
       ifstream infile("lastHash.dat", ios::binary);
@@ -106,42 +110,41 @@ class Blockchain {
       return hash == lastHash;
     }
     
-    void addBlock(Temp* data) throw (string) 
-    {
-      if(!verify()) 
-      {
+    //adds block to the end, throws string exception if blockchain was compromised TODO:rework
+    void addBlock(T* data) throw (string) {
+      //check chain's integrity
+      if(!verify()) {
         throw "Blockchain was compromised.\n";
       }
-      mesg_tail->mesg_next = new Block<Temp>(mesg_tail->mesg_hash, data, 0);
-      mesg_tail = mesg_tail->mesg_next;
+      //tail's next becomes new block with the tail's hash as previous hash, and it is pointing to null
+      m_tail->m_next = new Block<T>(m_tail->m_hash, data, 0);
+      //new block is the new tail
+      m_tail = m_tail->m_next;
+      //write new last hash to file
       string lastHash = getLastHash();
       ofstream outfile("lastHash.dat", ios::binary);
       outfile.write(lastHash.c_str(),lastHash.size());
       outfile.close();
-      mesg_size++;
+      m_size++;
     }
 
-    Typ* toArray() {
-      Temp* arr=new Temp[mesg_size];
-      if(mesg_head->mesg_next != 0) 
-      {
-        Block<Typ> *current = mesg_head->mesg_next;
-        for(int i=0;i<mesg_size;i++) 
-        {
-          arr[i] = *(current->mesg_data);
-          current = current->mesg_next;
+    T* toArray() {
+      T* arr=new T[m_size];
+      if(m_head->m_next != 0) {
+        Block<T> *current = m_head->m_next;
+        for(int i=0;i<m_size;i++) {
+          arr[i] = *(current->m_data);
+          current = current->m_next;
         }
       }
       return arr;      
     }
 
-    friend ostream& operator<<(ostream& os, const Blockchain<Temp>& b)
-    {
-      Block<Temp> *current = (b.mesg_head)->mesg_next;
-      while(current!= 0)
-      {
+    friend ostream& operator<<(ostream& os, const Blockchain<T>& b){
+      Block<T> *current = (b.m_head)->m_next;
+      while(current!= 0){
         os << *(current) << "\n";
-        current = current->mesg_next;
+        current = current->m_next;
       }
       return os;
     }
