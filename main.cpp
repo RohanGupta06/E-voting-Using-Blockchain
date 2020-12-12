5f5c021ef5b22db4c9dbfb2bb7fbf7ca927650b3
Rohan gupta <500067051@stu.upes.ac.in>
2020-12-12 23:56:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,4 @@
-/* Final Project
- 
-1. Create Blockchain class:
-  1. private inner class Block:
-    - stores previous hash, current hash, generic data, as well as the link (pointer) to the next block.
-    - default constructor creates genesis block: previous hash is the hash of "genesis block", data is empty generic object. Current hash is calculated in normal way.
-    - full constructor creates the block from provided previous hash, data and the link to the next block.
-    - destructor destroys data pointer
-    - calcHash(previousHash) recalculates the hash of the block with the new previous hash (used when validating the chain).
-    - getData() returns value of data 
-    - << operator overload prints information about the block
-  2. contains references (pointers) to head and tail as well as size (does not include genesis block)
-  3. Default constructor:
-    - use default constructor from Block to create a genesis block and point head and tail to it
-    - call getLastHash and output it into the binary file representing the database (or other users to compare to)
-  4. getSize returns the size (size)
-  5. getLastHash() traverses through the linked list to recalculate all the hashes:
-    - sets current block and current hash
-    - while the end of list not reached: 
-      - call the calcHash on current.next with current hash
-      - move current to the next block
-    return the last hash (current's hash)
-  6. verify() compares last hash of blockchain with database's hash:
-    - getLastHash() into variable
-    - read the hash from binary file
-    - return the result of comparison
-  7. addBlock(data) adds new block to the end of the chain:
-    - throws a string exception if verify() failed.
-    - set tail's next to the new block that takes tail's 
-    - repoint tail to the tail's next (new block is the new tail)
-    - getLastHash() and put it into the binary file (database)
-    - increment the size
-  8. toArray() returns a pointer to the array that contains the data from all blocks excluding genesis:
-    - traverse the blockchain in a similar way to getLastHash() and put each data point to the array
-    - return the array
-  9. << operator overload:
-    - prints all blocks excluding genesis
-2. Test Drive:
-  1. printResults() takes in the array of results and array of names and prints the winner and the percentage:
-    - sort the array
-    - count the repeated items
-    - if the counter reached a new max - set max to the counter. save the value that corresponds to max.
-    - when reached the new item, reset the counter.
-    - print the message
-  2. main (demo):
-    - create a blockchain object
-    - create appropriate variables
-    - in the loop:
-      - prompt the user if they want to enter the vote or tally the votes
-      - if enter the vote, add a block with the choice to the blockchain
-      - if tally the votes, call toArray on blockchain and print the message of received array.
-      - allow user to exit whenever they'd like
-*/
+
 #include <iostream>
 #include <cstring>
 #include <iomanip>
@@ -150,8 +98,8 @@ while(true) {
 
 void printResults(string *arr, int size, string names[],int sizeNames){
   sort(arr, arr+size);
-  static int max=0;
-  static int count=1;
+  int max=0;
+  int count=1;
   string current=arr[0], maxResult=arr[0];
   for(int i=1;i<size;i++){
     if(current==arr[i]){
